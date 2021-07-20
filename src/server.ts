77d7ea99a61ec951fb29695f06881de633939153
Nathally Souza <nathallyccd@gmail.com>
2021-07-20 16:28:55 -0300

    Instalando CORS

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 
 import { router } from "./routes";
 
 import "./database";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(router)
